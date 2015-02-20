f5ba9caaabbcf1fc20ad8d8bdfda5ba8dfba5bd7
Jason Diehl <jdiehl@exacttarget.com>
2015-02-20 10:48:21 -0500

    navigation modifications

--- public/js/views/aside.js
+++ public/js/views/aside.js
@@ -1,6 +1,7 @@
 define( function( require ) {
 
 	var Marionette = require( 'marionette' );
+	var Moment     = require( 'moment' );
 
 	var tmplAside = require( 'text!/templates/aside.html' );
 
@@ -8,18 +9,45 @@ define( function( require ) {
 		template: _.template( tmplAside )
 		, ui: {
 			'sideNavigation': '.side-navigation'
+			, 'btn': '.btn'
+			, 'btnGroup': 'btn-group'
+			, 'annualSalesYears': '.annual-sales-years'
+			, 'monthlySalesYears': '.monthly-sales-years'
+			, 'dropdownMenu': '.dropdown-menu'
+		}
+		, events: {
+			'click @ui.btn': 'navigate'
 		}
 		, initialize: function() {
 			
 		}
+		, className: 'row'
 		, onRender: function() {
-			this.$el.html( this.template( options ) );
-			// this.ui.sideNavigation.affix({
-			// 	offset: {
-			// 		top: 50
-			// 	}
-			// });
-			return this;
+			var width;
+			this.buildYearsNav();
+			
+			_.delay( _.bind( function() {
+				width = this.$el.parent().outerWidth();
+				height = this.ui.btn.outerHeight();
+				this.ui.btn.css( 'width', width-20 );
+				this.ui.dropdownMenu.css( {
+					'margin-left': width-20
+					, 'margin-top': height*-1
+					, 'z-index': 5000
+				} )
+			}, this ), 100 );
+		}
+		, navigate: function( $event ) {
+			window.location = $event.currentTarget.dataset.link;
+		}
+		, buildYearsNav: function() {
+			var year = moment().year();
+
+			while( year > 2007 ) {
+				this.ui.annualSalesYears.append( '<li><a href="#/reporting/annual/'+ year +'">'+ year +'</a></li>' );
+				this.ui.monthlySalesYears.append( '<li><a href="#/reporting/monthly/'+ year +'">'+ year +'</a></li>' );
+				year = moment( year, 'YYYY' ).subtract( 1, 'year').year();
+			}
 		}
 	});
 	return AsideView;
